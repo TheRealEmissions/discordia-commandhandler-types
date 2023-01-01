135beac2e7b36746548dc11ab6b093b754484fb2
therealemissions <me@liammartin.site>
2023-01-01 14:29:20 +0000

    Initial commit

---
+++ command-handler/src/App.d.ts
@@ -0,0 +1,6 @@
+import BaseApp from "./BaseApp.js";
+declare class App extends BaseApp {
+    constructor();
+    init(): void;
+}
+export default App;
---
+++ command-handler/src/BaseApp.d.ts
@@ -0,0 +1,14 @@
+import { HeadFile } from "ts-modular-bot-file-design";
+import { Dependency } from "ts-modular-bot-types";
+import Events from "events-types";
+declare abstract class BaseApp extends HeadFile {
+    constructor();
+    type: Dependency;
+    name: string;
+    load: boolean;
+    static Client: object;
+    static Events: typeof Events;
+    abstract init(): void;
+    getDependencies(): Dependency[];
+}
+export default BaseApp;
