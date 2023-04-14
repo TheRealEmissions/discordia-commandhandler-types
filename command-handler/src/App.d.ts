c447c09e5bcbb83608cf518b6c92b8454b3275b0
Emissions <unitedrealm@hotmail.com>
2023-04-14 18:50:28 +0100

    updated

--- command-handler/src/App.d.ts
+++ command-handler/src/App.d.ts
@@ -2,6 +2,7 @@ import BaseApp from "./BaseApp.js";
 import CommandConstructor from "./CommandConstructor.js";
 declare class App extends BaseApp {
     CommandConstructor: CommandConstructor;
+    Builders: typeof CommandConstructor;
     constructor();
     init(): Promise<void>;
 }
